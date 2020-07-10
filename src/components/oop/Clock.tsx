d83063c325c9113d860234b827685913d08c022e
강예림 <62981035+kangyerim@users.noreply.github.com>
2020-07-10 18:31:09 +0900

    Update Clock.tsx

--- src/components/oop/Clock.tsx
+++ src/components/oop/Clock.tsx
@@ -1,5 +1,50 @@
-import React, {Component} from "react";
+import React, {useState, useEffect} from "react";
+function Clock() {
+    const [tick, setTick] = useState(new Date().toLocaleTimeString());
+    useEffect(() => {
+        setInterval(() => setTick(new Date().toLocaleTimeString()), 1000)
+    });
+    return (
+        <div>
+            <h1>Hello World</h1>
+            <h2>Current Time</h2>
+            <h2>{tick}</h2>
+        </div>
+    );
+}
+export default Clock;
 
+/*import React, {useReducer} from "react";
+import reducers from "../../reducers";
+
+type Action = { type: 'clockStart' }
+function reducer(state: string, action: Action) {
+    switch (action.type) {
+        case 'clockStart': return tick()
+        default: throw new Error('unhanced action')
+    }
+}
+function tick() { //호이스팅 선언식: 고차함수에 프롭스로 들어올 순수함수는 호이스팅이 걸려있어야한다
+    setInterval(tick, 1000)
+    setState({
+        time : new Date().toLocaleTimeString()
+    })
+}
+const Clock: React.FC = () => { // 표현식 고차함수
+    const [time, dispatch ] = useReducer(reducer, "")
+    const clockStart = () => dispatch({ type: 'clockStart'})
+    return <div>
+            <h2>아무것도 안했는ㄴ데 벌써.., {time} 야.,.,</h2>
+            <button onClick={clockStart}>tick tok</button>
+        </div>
+}
+
+export default Clock*/
+
+
+
+/*
+import React, {Component} from "react";
 
 export default class Clock extends Component{
     public state = {time : ''}
@@ -18,4 +63,4 @@ export default class Clock extends Component{
         </div>
     }
 
-}
+}*/
